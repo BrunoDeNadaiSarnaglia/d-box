ee0bca88c254614ebc1598c746c7f7f12dd1ff46
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2015-04-21 02:11:38 -0500

    second commit

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -15,13 +18,74 @@ import javax.servlet.http.HttpServletResponse;
     )
 public class HelloServlet extends HttpServlet {
 
-    @Override
+    /*@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         out.write("hello heroku".getBytes());
         out.flush();
         out.close();
+    }*/
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        if (req.getRequestURI().endsWith("/db")) {
+            showDatabase(req,resp);
+        } else {
+            showHome(req,resp);
+        }
+    }
+
+    private void showHome(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"\n" +
+                "   \"http://www.w3.org/TR/html4/strict.dtd\">\n" +
+                "<HTML>\n" +
+                "   <HEAD>\n" +
+                "      <TITLE>My first HTML document</TITLE>\n" +
+                "   </HEAD>\n" +
+                "   <BODY>\n" +
+                "      <P>Hello world!\n" +
+                "   </BODY>\n" +
+                "</HTML>");
+    }
+
+    private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Connection connection = null;
+        try {
+            connection = getConnection();
+
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+            String out = "Hello!\n";
+            while (rs.next()) {
+                out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
+            }
+
+            resp.getWriter().print(out);
+        } catch (Exception e) {
+            resp.getWriter().print("There was an error: " + e.getMessage());
+        } finally {
+            if (connection != null) try{connection.close();} catch(SQLException e){}
+        }
+    }
+
+    private Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        int port = dbUri.getPort();
+
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
     }
     
 }
