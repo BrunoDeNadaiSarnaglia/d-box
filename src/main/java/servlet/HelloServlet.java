94bc130fb322cd3a74cde2491280e9a6e4f3f9bf
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2015-04-21 02:18:04 -0500

    third commit

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet", 
-        urlPatterns = {"/hello"}
+        urlPatterns = {"/db"}
     )
 public class HelloServlet extends HttpServlet {
 
