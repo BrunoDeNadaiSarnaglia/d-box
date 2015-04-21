2a9cab3671fa0ebc89ec11ce05fcb4efefb22e27
Cassio Sousa <cassiosantossousa@gmail.com>
2015-04-21 17:56:09 -0500

    Commented useless code

--- src/main/java/servlet/SignInRequest.java
+++ src/main/java/servlet/SignInRequest.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @author Cassio
@@ -31,8 +30,8 @@ public class SignInRequest extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        /*
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -42,7 +41,7 @@ public class SignInRequest extends HttpServlet {
             out.println("<h1>Servlet RequestSignIn at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
-        }
+        }*/
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
