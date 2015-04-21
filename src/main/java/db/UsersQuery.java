d5c061d13579d8598a0f82946ad9afbb96f7100c
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2015-04-21 18:01:06 -0500

    Query username by email

---
+++ src/main/java/db/UsersQuery.java
@@ -0,0 +1,53 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created by Bruno on 4/21/2015.
+ */
+public class UsersQuery {
+
+    public static String getUsername(String email){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = ConnectionConfigure.getConnection();
+            preparedStatement = connection.prepareStatement("SELECT username FROM users WHERE email = ?");
+            preparedStatement.setString(1, email);
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return resultSet.getString("username");
+            } else{
+                return null;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally  {
+            if(preparedStatement != null){
+                try {
+                    preparedStatement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null){
+                try{
+                    connection.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null){
+                try{
+                    resultSet.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+}
