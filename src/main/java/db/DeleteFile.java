2d7311c86185b2540e234edc798c7528d3690a73
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2015-04-24 01:58:10 -0500

    delete file

---
+++ src/main/java/db/DeleteFile.java
@@ -0,0 +1,39 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ * Created by Bruno on 4/24/2015.
+ */
+public class DeleteFile {
+
+    public static void delete(Integer id){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try{
+            connection = ConnectionConfigure.getConnection();
+            preparedStatement = connection.prepareStatement("DELETE FROM file WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(preparedStatement != null){
+                try {
+                    preparedStatement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null){
+                try{
+                    connection.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
