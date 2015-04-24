76aa6e844d54f42167736e8e681b029b02eca1a2
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2015-04-24 00:53:12 -0500

    listing folders inside a folder

--- src/main/java/db/ListFoldersInFolder.java
+++ src/main/java/db/ListFoldersInFolder.java
@@ -16,16 +16,9 @@ import java.util.HashSet;
  */
 public class ListFoldersInFolder {
 
-<<<<<<< HEAD:src/main/java/db/ListFoldersInFolder.java
     private static HashSet<Folder> setOfFolders = new HashSet<Folder>();
 
-
-    public static void query(Integer id){
-=======
-    private static HashSet<Integer> setOfIds = new HashSet<Integer>();
-
-    public static void add(Integer id) {
->>>>>>> 42eaa3a2489a5ac592ff9d0c26a10a4defa8d57b:src/main/java/db/ListContentInFolder.java
+    public static void list(Integer id) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -34,18 +27,9 @@ public class ListFoldersInFolder {
             preparedStatement = connection.prepareStatement("SELECT id, name FROM contain, folder WHERE parentId = ? AND childId = id");
             preparedStatement.setInt(1, id);
             resultSet = preparedStatement.executeQuery();
-<<<<<<< HEAD:src/main/java/db/ListFoldersInFolder.java
             while(resultSet.next()) {
-                int idChild= resultSet.getInt("childId");
-                setOfFolders.add(new Folder(resultSet.getInt("id"), resultSet.getString("name")));
-=======
-            while (resultSet.next()) {
                 int idChild = resultSet.getInt("childId");
-                setOfIds.add(idChild);
-            }
-            for (Integer idChild : setOfIds) {
-
->>>>>>> 42eaa3a2489a5ac592ff9d0c26a10a4defa8d57b:src/main/java/db/ListContentInFolder.java
+                setOfFolders.add(new Folder(resultSet.getInt("id"), resultSet.getString("name")));
             }
         } catch (Exception e) {
             e.printStackTrace();
