d844657525182536a1751fdc75026cc8a13d5f4d
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2015-04-24 02:21:42 -0500

    delete account

---
+++ src/main/java/db/DeleteAccount.java
@@ -0,0 +1,51 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created by Bruno on 4/24/2015.
+ */
+public class DeleteAccount {
+
+    public static void delete(String email){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = ConnectionConfigure.getConnection();
+            preparedStatement = connection.prepareStatement("DELETE FROM friendship WHERE email1 = ? OR email2 = ?");
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, email);
+            preparedStatement.executeUpdate();
+            preparedStatement = connection.prepareStatement("SELECT id FROM users WHERE email = ?");
+            preparedStatement.setString(1, email);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                int id = resultSet.getInt("id");
+                DeleteFolder.delete(id);
+                preparedStatement = connection.prepareStatement("DELETE FROM users WHERE email = ?");
+                preparedStatement.setString(1, email);
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(preparedStatement != null){
+                try {
+                    preparedStatement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null){
+                try{
+                    connection.close();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
